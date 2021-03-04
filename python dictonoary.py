4376202c30a636f891ff1bf31c38fd5c01956172
sheharaashraf <76677381+sheharaashraf@users.noreply.github.com>
2021-03-04 12:07:13 +0530

    Add files via upload

--- python dictonoary.py
+++ python dictonoary.py
@@ -1,19 +1,55 @@
 import csv
-csv_col=['ID:','Name','Place']
-dict_data=[{'ID':1,'Name':'Alex','Place':'Muvattupuzha'}],
-{'ID':2,'Name':'Shehara','Place':'Perumbavoor'}
-{'ID':3,'Name':'Shri Ram','Place':'alpuzha'}
-{'ID':4,'Name':'Smith','Place':'Pathanamthitta'}
+
+csv_col=['ID','Name','Place']
+
+dict_data=[{'ID':1,'Name':'Anjana','Place':'thrissur'},
+
+           {'ID':2,'Name':'anagha','Place':'wayanad'},
+
+           {'ID':3,'Name':'aiswarya','Place':'kannur'},
+
+           {'ID':4,'Name':'swathisha','Place':'thrissur'},
+
+           {'ID':5,'Name':'swetha','Place':'kannur'},
+
+           {'ID':6,'Name':'salima','Place':'kannur'},
+
+           {'ID':7,'Name':'kavya','Place':'kottayam'},
+
+           {'ID':8,'Name':'selmi','Place':'kottayam'},
+
+           {'ID':9,'Name':'adhithya','Place':'kannur'},
+
+           {'ID':10,'Name':'anjanakp','Place':'palakkad'},]
+
+
+
 csv_file="Names.csv"
+
 try:
-    with open("names.csv",'w')as file1:
-        writer=csv.dictWriter(file,fieldnames=csv_col)
+
+    with open(csv_file,'w')as file1:
+
+        writer=csv.DictWriter(file1,fieldnames=csv_col)
+
         writer.writeheader()
+
         for data1 in dict_data:
+
             writer.writerow(data1)
+
+
+
 except IOError:
-                print("I/O error")
-                data1=csv.dictReader(open(csv_file))
-                print("CSV file as a dictonary:\n")
+
+    print("ioeror")
+
+
+
+data1=csv.DictReader(open(csv_file))
+
+print("csv file as dictionary:\n")
+
 for row in data1:
-                      print(row)
+
+    print(row)
